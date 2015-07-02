5ff09af654e785e2f6d1466ca7fa6d054c6de27c
Belkacem Alidra <belkacem.alidra@gmail.com>
2015-07-02 10:24:26 +0200

    Add custom base-'8encode function

--- controllers/add.js
+++ controllers/add.js
@@ -6,6 +6,7 @@ var data		= args.data;
 if (!_.isEmpty(data)) {
 	$.add_torrent_description.hide();
 	$.url.hide();
+	data = base64_encode(data);
 }
 
 $.destination.value = Ti.App.Properties.getString('last_movie_download_dir');
@@ -67,3 +68,58 @@ $.add_button.addEventListener('click', function() {
 		}	
 	);
 });
+
+/**
+ * Encodes the data in base64
+ * We don't use the Titanium encoder, because it produces an error with Transmission ;(
+ * 	
+ * @see http://phpjs.org/functions/base64_encode/
+ * 
+ * @param {Object} data
+ */
+function base64_encode(data) {
+  //  discuss at: http://phpjs.org/functions/base64_encode/
+  // original by: Tyler Akins (http://rumkin.com)
+  // improved by: Bayron Guevara
+  // improved by: Thunder.m
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Rafał Kukawski (http://kukawski.pl)
+  // bugfixed by: Pellentesque Malesuada
+  //   example 1: base64_encode('Kevin van Zonneveld');
+  //   returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+  //   example 2: base64_encode('a');
+  //   returns 2: 'YQ=='
+
+  var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+    ac = 0,
+    enc = '',
+    tmp_arr = [];
+
+  if (!data) {
+    return data;
+  }
+
+  do { // pack three octets into four hexets
+    o1 = data.charCodeAt(i++);
+    o2 = data.charCodeAt(i++);
+    o3 = data.charCodeAt(i++);
+
+    bits = o1 << 16 | o2 << 8 | o3;
+
+    h1 = bits >> 18 & 0x3f;
+    h2 = bits >> 12 & 0x3f;
+    h3 = bits >> 6 & 0x3f;
+    h4 = bits & 0x3f;
+
+    // use hexets to index into b64, and append result to encoded string
+    tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+  } while (i < data.length);
+
+  enc = tmp_arr.join('');
+
+  var r = data.length % 3;
+
+  return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
+}
